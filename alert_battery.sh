ab0a9769d93edd2bde85cc191364bac1ada3342b
subpath <code@subpath.de>
2017-03-12 21:15:48 +0100

    rewrite alert battery in tcsh

--- alert_battery.sh
+++ alert_battery.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-status=$(acpi | grep -o [0-9][0-9]*% | grep -o [0-9]*)
-bat=$(acpi | grep -o [A-Z][a-z]*argi[a-z]*)
-if [ "$bat" == 'Discharging' ]; then 
-		if [ "$status" -le 10 ]; then 
-				beep $(cat /home/subpath/.skript/bash/music)
-				#ratpoison -c "echo battery is low"
-		fi
-fi
+#!/bin/tcsh
+
+set stat = `acpi | grep -o '\([0-9][0-9]*%\)' | grep -o "\([0-9]*\)"`
+set bat = `acpi | grep -o '\([A-Z][a-z]*argi[a-z]*\)'`
+if ( $bat == 'Discharging') then 
+	if ( $stat < 10 ) then
+		#beep `cat /home/subpath/.skript/bash/music`
+		ratpoison -c "echo empty battery"
+	endif
+endif
+if ( $stat > 90) then
+	ratpoison -c "echo battery is full"
+endif
